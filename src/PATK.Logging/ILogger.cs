5e447f06d866eb7bc2d52d5fa438d40eaee2bcf8
Michael Rose <mik.rose@outlook.com>
2015-10-29 19:05:08 +0000

    Added logging interface and concrete implementation using serilog and nlog

---
+++ src/PATK.Logging/ILogger.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace PATK.Logging
+{
+    /// <summary>
+    /// Interface for logging events, errors, warnings etc.
+    /// </summary>
+    public interface ILogger
+    {
+        void Error(string format, params object[] args);
+        void Error(string message);
+        void Error(Exception ex, string format, params object[] args);
+        void Information(string message);
+        void Information(string format, params object[] args);
+        void Trace(string message);
+        void Trace(string format, params object[] args);
+        void Warning(string message);
+        void Warning(string format, params object[] args);
+        void Debug(string message);
+        void Debug(string format, params object[] args);
+    }
+}
---
+++ src/PATK.Logging/Logger.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using Serilog;
+
+namespace PATK.Logging
+{
+    /// <summary>
+    /// Implementation of <see cref="ILogger"/> using serilog and nlog
+    /// </summary>
+    public class Logger : ILogger
+    {
+        private readonly Serilog.ILogger _logger;
+
+        public Logger()
+        {
+            _logger = new LoggerConfiguration().WriteTo.NLog().CreateLogger();
+        }
+
+        public void Error(string format, params object[] args)
+        {
+            _logger.Error(format, args);
+        }
+
+        public void Error(string message)
+        {
+            _logger.Error(message);
+        }
+
+        public void Error(Exception ex, string format, params object[] args)
+        {
+            _logger.Error(ex, format, args);
+        }
+
+        public void Information(string message)
+        {
+            _logger.Information(message);
+        }
+
+        public void Information(string format, params object[] args)
+        {
+            _logger.Information(format, args);
+        }
+
+        public void Trace(string message)
+        {
+            _logger.Verbose(message);
+        }
+
+        public void Trace(string format, params object[] args)
+        {
+            _logger.Verbose(format, args);
+        }
+
+        public void Warning(string message)
+        {
+            _logger.Warning(message);
+        }
+
+        public void Warning(string format, params object[] args)
+        {
+            _logger.Warning(format, args);
+        }
+
+        public void Debug(string message)
+        {
+            _logger.Debug(message);
+        }
+
+        public void Debug(string format, params object[] args)
+        {
+            _logger.Debug(format, args);
+        }
+    }
+}
