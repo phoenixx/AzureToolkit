2957419e4c0bdb86d58d22d38ad0bea6c3e47374
Michael Rose <mik.rose@outlook.com>
2015-10-29 18:24:11 +0000

    Added common solution for shared classes

---
+++ src/PATK.Common/Constants/Azure.cs
@@ -0,0 +1,8 @@
+﻿namespace PATK.Common.Constants
+{
+    public static class Azure
+    {
+        public const string PublishSettingsDownloadPath =
+            "https://manage.windowsazure.com/publishsettings/index?client=xplat";
+    }
+}
