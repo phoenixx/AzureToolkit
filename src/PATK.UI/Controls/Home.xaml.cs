6ccd45a18ee86124b64aec6ef03d4f5347a08d9e
mike_rose <mik.rose@outlook.com>
2015-11-02 20:57:43 +0000

    Slow progress on getting rdp fie

--- src/PATK.UI/Controls/Home.xaml.cs
+++ src/PATK.UI/Controls/Home.xaml.cs
@@ -5,6 +5,7 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 using Microsoft.Win32;
+using Microsoft.WindowsAzure.Management.Compute.Models;
 using PATK.Common.XML;
 using PATK.Rest.Repositories;
 using PATK.UI.Commands;
@@ -18,98 +19,23 @@ namespace PATK.UI.Controls
     /// </summary>
     public partial class Home : UserControl
     {
-        private readonly IPublishSettingsReader _publishSettingsReader;
-        private readonly ICloudServicesRepository _cloudServicesRepository;
         private ServicesViewModel _viewModel;
-        private bool _subscriptionLoaded = false;
 
         public Home()
         {
-            _publishSettingsReader = Container.Get<IPublishSettingsReader>();
-            _cloudServicesRepository = Container.Get<ICloudServicesRepository>();
-            LoadSubscriptionCommand = new DelegateCommand(LoadSubscription, CanLoadSubscription);
-            _viewModel = new ServicesViewModel()
-            {
-                WindowCommand = LoadSubscriptionCommand
-            };
-
+            _viewModel = new ServicesViewModel();
             DataContext = _viewModel;
             InitializeComponent();
-
         }
 
-        public ICommand LoadSubscriptionCommand { get; }
-
-        private void LoadSubscription(object state)
+        private void GetRdpFile(object sender, RoutedEventArgs e)
         {
-            const string filter = "Azure Publish Settings|*.publishSettings";
-
-            var ofd = new OpenFileDialog()
+            var button = sender as Button;
+            if (button != null)
             {
-                Filter = filter,
-                Title = "Select your publish settings file"
-            };
-
-            var selected = ofd.ShowDialog();
-
-            if (selected.HasValue && selected.Value)
-            {
-                var filename = ofd.FileName;
-                var fileContents = File.ReadAllBytes(filename);
-                var subscription = _publishSettingsReader.ReadPublishSettings(fileContents);
-
-                if (subscription != null)
-                {
-                    Common.Global.Settings.PublishSettings = subscription.First();
-                    _subscriptionLoaded = true;
-                    LoadCloudServices();
-                }
+                var service = ((HostedServiceListResponse.HostedService)button.DataContext);
+                var t = service.ComputeCapabilities.VirtualMachinesRoleSizes;
             }
-        }
-
-        private bool CanLoadSubscription(object state)
-        {
-            // return true/false here is enabled/disable button
-            return true;
-        }
-
-        private void LoadSubscription_OnClick(object sender, RoutedEventArgs e)
-        {
-            const string filter = "Azure Publish Settings|*.publishSettings";
-
-            var ofd = new OpenFileDialog()
-            {
-                Filter = filter,
-                Title = "Select your publish settings file"
-            };
-
-            var selected = ofd.ShowDialog();
-
-            if (selected.HasValue && selected.Value)
-            {
-                var filename = ofd.FileName;
-                var fileContents = File.ReadAllBytes(filename);
-                var subscription = _publishSettingsReader.ReadPublishSettings(fileContents);
-
-                if (subscription != null)
-                {
-                    Common.Global.Settings.PublishSettings = subscription.First();
-                    _subscriptionLoaded = true;
-                    LoadCloudServices();
-                }
-            }
-        }
-
-        private async void LoadCloudServices()
-        {
-            if (!_subscriptionLoaded)
-            {
-                return;
-            }
-
-            var cloudServices = await _cloudServicesRepository.GetCloudServices();
-            _viewModel.CloudServices = cloudServices.HostedServices;
-            DataContext = _viewModel;
         }
     }
 }
--- src/PATK.UI/Viewmodels/ServicesViewModel.cs
+++ src/PATK.UI/Viewmodels/ServicesViewModel.cs
@@ -1,14 +1,61 @@
 ﻿using System.Collections.Generic;
 using System.ComponentModel;
+using System.ComponentModel.Design;
+using System.IO;
+using System.Linq;
 using System.Runtime.CompilerServices;
+using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Input;
+using Microsoft.Win32;
 using Microsoft.WindowsAzure.Management.Compute.Models;
+using PATK.Common.XML;
+using PATK.Rest.Repositories;
 using PATK.UI.Annotations;
+using PATK.UI.Commands;
+using Container = PATK.UI.DI.Container;
 
 namespace PATK.UI.Viewmodels
 {
     public class ServicesViewModel : INotifyPropertyChanged
     {
+        private readonly IPublishSettingsReader _publishSettingsReader;
+        private readonly ICloudServicesRepository _cloudServicesRepository;
+        private HostedServiceListResponse.HostedService _selectedService;
+
+        public ServicesViewModel()
+        {
+            _publishSettingsReader = Container.Get<IPublishSettingsReader>();
+            _cloudServicesRepository = Container.Get<ICloudServicesRepository>();
+            LoadSubscriptionCommand = new DelegateCommand(LoadSubscription, CanLoadSubscription);
+            ClearSubscriptionCommand = new DelegateCommand(ClearSubscription, CanClearSubscription);
+        }
+
+        private bool _subscriptionLoaded;
+
+        public bool SubscriptionLoaded
+        {
+            get { return _subscriptionLoaded; }
+            set
+            {
+                _subscriptionLoaded = value;
+                NotifyPropertyChanged("SubscriptionLoaded");
+            }
+        }
+
+        public HostedServiceListResponse.HostedService SelectedService
+        {
+            get
+            {
+                return _selectedService;
+            }
+            set
+            {
+                _selectedService = value;
+                NotifyPropertyChanged("SelectedService");
+            }
+        }
+
         public event PropertyChangedEventHandler PropertyChanged;
 
         private IList<HostedServiceListResponse.HostedService> _cloudServices;
@@ -23,7 +70,52 @@ namespace PATK.UI.Viewmodels
             }
         }
 
-        public ICommand WindowCommand { get; set; }
+        public ICommand LoadSubscriptionCommand { get; internal set; }
+        public ICommand ClearSubscriptionCommand { get; internal set; }
+        public ICommand SelectServiceCommnd { get; internal set; }
+
+        private void LoadSubscription(object state)
+        {
+            const string filter = "Azure Publish Settings|*.publishSettings";
+
+            var ofd = new OpenFileDialog()
+            {
+                Filter = filter,
+                Title = "Select your publish settings file"
+            };
+
+            var selected = ofd.ShowDialog();
+
+            if (selected.HasValue && selected.Value)
+            {
+                var filename = ofd.FileName;
+                var fileContents = File.ReadAllBytes(filename);
+                var subscription = _publishSettingsReader.ReadPublishSettings(fileContents);
+
+                if (subscription != null)
+                {
+                    Common.Global.Settings.PublishSettings = subscription.First();
+                    SubscriptionLoaded = true;
+                    LoadCloudServices();
+                }
+            }
+        }
+
+        private bool CanLoadSubscription(object state)
+        {
+            return !_subscriptionLoaded;
+        }
+
+        private void ClearSubscription(object state)
+        {
+            SubscriptionLoaded = false;
+            CloudServices = null;
+        }
+
+        private bool CanClearSubscription(object state)
+        {
+            return _subscriptionLoaded;
+        }
 
         private void NotifyPropertyChanged(string property)
         {
@@ -34,6 +126,17 @@ namespace PATK.UI.Viewmodels
         protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private async void LoadCloudServices()
+        {
+            if (!SubscriptionLoaded)
+            {
+                return;
+            }
+
+            var cloudServices = await _cloudServicesRepository.GetCloudServices();
+            CloudServices = cloudServices.HostedServices;
         }
     }
 }
